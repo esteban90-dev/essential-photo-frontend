b68baa21fc1b025ca88090da418b4e9064c81421
esteban90-dev <stephencallard@gmail.com>
2022-08-17 09:36:19 -0600

    display Not Found page if route doesn't exist

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Login from './Pages/Login';
 import AdminImages from './Pages/AdminImages';
+import NotFound from './Pages/NotFound';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>}></Route>
         <Route path="/admin/images" element={isLoggedIn ? <AdminImages /> : <Login setIsLoggedIn={setIsLoggedIn}/>}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   )
