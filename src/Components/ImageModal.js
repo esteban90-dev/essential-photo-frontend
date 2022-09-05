5437a5387a078b1671874472748347d1411341fa
esteban90-dev <stephencallard@gmail.com>
2022-09-05 14:44:52 -0600

    make image modal responsive

--- src/Components/ImageModal.js
+++ src/Components/ImageModal.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ModalAdvancer from './ModalAdvancer';
+import React, { useEffect } from 'react';
 import leftArrowIcon from '../images/left-arrow-icon.svg';
 import rightArrowIcon from '../images/right-arrow-icon.svg';
+import {MODAL_ADVANCE_OVERLAY_WIDTH} from '../settings';
 
 export default function ImageModal(props) {
+  const [leftOverlayIsHovered, setLeftOverlayIsHovered] = React.useState(false);
+  const [rightOverlayIsHovered, setRightOverlayIsHovered] = React.useState(false);
+
   const modalRef = React.useRef(null);
+  const imageRef = React.useRef(null);
+  const leftOverlayRef = React.useRef(null);
+  const rightOverlayRef = React.useRef(null);
 
   function handleClick(event) {
     // when the modal area outside of the image
@@ -14,24 +20,109 @@ export default function ImageModal(props) {
     }
   }
 
+  useEffect(() => {
+    // do the following when the modal first loads and when the
+    // window is resized:
+    // 1. resize image to fit the screen
+    // 2. place overlays on left and right sides of the image
+
+    const imageElement = imageRef.current;
+    const leftOverlayElement = leftOverlayRef.current;
+    const rightOverlayElement = rightOverlayRef.current;
+
+    function resizeImage() {
+      const imageAspectRatio = imageRef.current.naturalWidth / imageRef.current.naturalHeight;
+      let windowAspectRatio = window.innerWidth / window.innerHeight;
+      
+      if (windowAspectRatio > imageAspectRatio) {
+        imageRef.current.style.height = '90vh';
+        imageRef.current.style.width = `${imageRef.current.getBoundingClientRect().height * imageAspectRatio}px`;
+      }
+      else {
+        imageRef.current.style.width = '90vw';
+        imageRef.current.style.height = `${imageRef.current.getBoundingClientRect().width / imageAspectRatio}px`;
+      }
+    }
+
+    function placeOverlays() {
+      const {
+        height: imageHeight,
+        width: imageWidth,
+        top: imageTop,
+        left: imageLeft,
+        right: imageRight,
+      } = imageElement.getBoundingClientRect();
+
+      leftOverlayElement.style.height = `${imageHeight}px`;
+      leftOverlayElement.style.width = `${imageWidth * MODAL_ADVANCE_OVERLAY_WIDTH}px`;
+      leftOverlayElement.style.top = `${imageTop}px`;
+      leftOverlayElement.style.left = `${imageLeft}px`;
+
+      rightOverlayElement.style.height = `${imageHeight}px`;
+      rightOverlayElement.style.width = `${imageWidth * MODAL_ADVANCE_OVERLAY_WIDTH}px`;
+      rightOverlayElement.style.top = `${imageTop}px`;
+      rightOverlayElement.style.left = 
+        `${imageRight - rightOverlayElement.getBoundingClientRect().width}px`;
+    }
+
+    imageElement.addEventListener('load', resizeImage);
+    imageElement.addEventListener('load', placeOverlays)
+    window.addEventListener('resize', resizeImage);
+    window.addEventListener('resize', placeOverlays);
+
+    return () => {
+      window.removeEventListener('resize', resizeImage);
+      window.removeEventListener('resize', placeOverlays);
+      imageElement.removeEventListener('load', resizeImage);
+      imageElement.removeEventListener('load', placeOverlays);
+    };
+  }, []);
+
   return (
     <div className="imageModal" onClick={handleClick} ref={modalRef}>
-      <div className="imageModal__imageContainer">
-        <ModalAdvancer
-          icon={leftArrowIcon}
-          position="left"
-          setNextImage={props.setPreviousImage}
-        />
-        <img
-          src={props.url}
-          className="imageModal__image"
-          alt=""
-        ></img>
-        <ModalAdvancer
-          icon={rightArrowIcon}
-          position="right"
-          setNextImage={props.setNextImage}
-        />
+      <img
+        src={props.url}
+        ref={imageRef}
+        className="imageModal__image"
+        alt=""
+      ></img>
+      <div
+        className={leftOverlayIsHovered ? 
+          `imageModal__overlay imageModal__overlay--hovered`
+          :
+          `imageModal__overlay`
+        }
+        onMouseEnter={() => setLeftOverlayIsHovered(true)}
+        onMouseLeave={() => setLeftOverlayIsHovered(false)}
+        ref={leftOverlayRef}
+        onClick={() => props.setPreviousImage()}
+      >
+        {leftOverlayIsHovered && 
+          <img
+            src={leftArrowIcon}
+            alt="this is an arrow icon"
+            className="imageModal__icon"
+          ></img>
+        }
+      </div>
+      <div
+        className={rightOverlayIsHovered ? 
+          `imageModal__overlay imageModal__overlay--hovered`
+          :
+          `imageModal__overlay`
+        }
+        onMouseEnter={() => setRightOverlayIsHovered(true)}
+        onMouseLeave={() => setRightOverlayIsHovered(false)}
+        ref={rightOverlayRef}
+        onClick={() => props.setNextImage()}
+      >
+        {rightOverlayIsHovered && 
+          <img
+            src={rightArrowIcon}
+            alt="this is an arrow icon"
+            className="imageModal__icon"
+          ></img>
+        }
       </div>
     </div>
   )
--- src/settings.js
+++ src/settings.js
@@ -4,3 +4,4 @@ export const LOGOUT_ENDPOINT = '/api/v1/auth/sign_out';
 export const POST_IMAGES_ENDPOINT = '/api/v1/images';
 export const IMAGES_INDEX_ENDPOINT = '/api/v1/images';
 export const DOMAIN_NAME = 'Essential Photo';
+export const MODAL_ADVANCE_OVERLAY_WIDTH = .15; // number is percentage of image width
