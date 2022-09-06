adbfc9411bcc79d69ff9dcd6e25cc865e947bae0
esteban90-dev <stephencallard@gmail.com>
2022-09-06 09:52:00 -0600

    add keyboard events to advance/close image modal

--- src/Components/ImageModal.js
+++ src/Components/ImageModal.js
@@ -21,10 +21,19 @@ export default function ImageModal(props) {
   }
 
   useEffect(() => {
-    // do the following when the modal first loads and when the
-    // window is resized:
-    // 1. resize image to fit the screen
-    // 2. place overlays on left and right sides of the image
+    // When the modal first loads, the image will be resized to 
+    // fit the screen and the advancer overlays will be placed 
+    // on either side of the image.
+    //
+    // Additionally, event listeners are attached to the window 
+    // object to resize the image and re-place the overlays
+    // whenever the window is resized.
+    //
+    // Additionally, event listeners are attached to the window
+    // object to close the modal whenever the escape key is pressed
+    // and load the next/previous image whenever the left/right 
+    // arrow keys are pressed
+    //
 
     const imageElement = imageRef.current;
     const leftOverlayElement = leftOverlayRef.current;
@@ -65,18 +74,37 @@ export default function ImageModal(props) {
         `${imageRight - rightOverlayElement.getBoundingClientRect().width}px`;
     }
 
+    function handleKeyDown(event) {
+      // if escape key was pressed
+      if (event.which === 27) {
+        props.closeModal();
+      }
+
+      // if right arrow key was pressed
+      else if (event.which === 39) {
+        props.setNextImage();
+      }
+
+      // if left arrow key was pressed
+      else if (event.which === 37) {
+        props.setPreviousImage();
+      }
+    }
+
     imageElement.addEventListener('load', resizeImage);
     imageElement.addEventListener('load', placeOverlays)
     window.addEventListener('resize', resizeImage);
     window.addEventListener('resize', placeOverlays);
+    window.addEventListener('keydown', handleKeyDown);
 
     return () => {
       window.removeEventListener('resize', resizeImage);
       window.removeEventListener('resize', placeOverlays);
+      window.removeEventListener('keydown', handleKeyDown);
       imageElement.removeEventListener('load', resizeImage);
       imageElement.removeEventListener('load', placeOverlays);
     };
-  }, []);
+  }, [props]);
 
   return (
     <div className="imageModal" onClick={handleClick} ref={modalRef}>
