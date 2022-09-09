d3c020f15ff256ef771a7bd83dc56c47adca3b51
esteban90-dev <stephencallard@gmail.com>
2022-09-09 16:44:22 -0600

    Add ability to swipe on modal image

--- src/settings.js
+++ src/settings.js
@@ -4,4 +4,11 @@ export const LOGOUT_ENDPOINT = '/api/v1/auth/sign_out';
 export const POST_IMAGES_ENDPOINT = '/api/v1/images';
 export const IMAGES_INDEX_ENDPOINT = '/api/v1/images';
 export const DOMAIN_NAME = 'Essential Photo';
-export const MODAL_ADVANCE_OVERLAY_WIDTH = .15; // number is percentage of image width
+
+// size of the overlays that appear when hovering over the image modal
+// (number is percentage of image width)
+export const MODAL_ADVANCE_OVERLAY_WIDTH = .15;
+
+// number of pixels that a swipe must move an image in order to trigger
+// loading the next image (number is in pixels)
+export const SWIPE_DISTANCE_THRESHOLD = 50;
