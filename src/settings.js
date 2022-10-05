5a45d61347e0cd3dd6e91f8c64f92fbcde55c2cd
esteban90-dev <stephencallard@gmail.com>
2022-10-05 11:35:07 -0600

    add endpoint that fetches all photos (including private)

--- src/settings.js
+++ src/settings.js
@@ -3,7 +3,8 @@ export const LOGIN_ENDPOINT = '/api/v1/auth/sign_in';
 export const LOGOUT_ENDPOINT = '/api/v1/auth/sign_out';
 export const POST_IMAGES_ENDPOINT = '/api/v1/images';
 export const UPDATE_IMAGE_ENDPOINT = '/api/v1/images';
-export const IMAGES_INDEX_ENDPOINT = '/api/v1/images';
+export const IMAGES_INDEX_ENDPOINT_PUBLIC_IMAGES_ONLY = '/api/v1/images';
+export const IMAGES_INDEX_ENDPOINT_ALL_IMAGES = '/api/v1/images?include_private=true'
 export const DOMAIN_NAME = 'Essential Photo';
 
 // size of the overlays that appear when hovering over the ShowImage component
