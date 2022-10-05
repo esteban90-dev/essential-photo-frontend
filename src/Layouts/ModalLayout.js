a3c52e7b371e560000905723122622e2bc76384e
esteban90-dev <stephencallard@gmail.com>
2022-10-05 10:44:05 -0600

    prevent user from accidentaly closing modal when the initial click originated on a different component

--- src/Layouts/ModalLayout.js
+++ src/Layouts/ModalLayout.js
@@ -3,11 +3,23 @@ import './ModalLayout.css';
 
 export default function ModalLayout(props) {
   const modalRef = React.useRef(null);
+  const [isMouseDownOnModal, setIsMouseDownOnModal] = React.useState(false);
 
-  function handleClick(event) {
+  function handleMouseDown(event) {
     if (event.target === modalRef.current) {
+      setIsMouseDownOnModal(true);
+    }
+  }
+
+  function handleMouseUp(event) {
+    // Only close the modal if the mouse down/up events both originated on the 
+    // modal.  Do this to prevent the user from inadvertently closing the modal
+    // when the they mouse down on a different component but 
+    // mouse up on the modal  
+    if ((event.target === modalRef.current) && isMouseDownOnModal) {
       props.close();
     }
+    setIsMouseDownOnModal(false);
   }
 
   function getClassName() {
@@ -18,7 +30,12 @@ export default function ModalLayout(props) {
   }
 
   return (
-    <div className={getClassName()} ref={modalRef} onClick={handleClick}>
+    <div
+      className={getClassName()}
+      ref={modalRef}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
       {props.children}
     </div>
   );
