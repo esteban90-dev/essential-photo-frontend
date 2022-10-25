76e6abc292b6bd22b3c7808bacbae95b520b16a5
esteban90-dev <stephencallard@gmail.com>
2022-10-25 11:16:21 -0600

    add search input element

--- src/Components/VisitorHeader.js
+++ src/Components/VisitorHeader.js
@@ -6,6 +6,16 @@ export default function VisitorHeader() {
   return (
     <header className="visitorHeader">
       <h1 className="visitorHeader__title">{DOMAIN_NAME}</h1>
+      <div className="visitorHeader__search">
+        <input
+          type="text"
+          className="visitorHeader__input"
+          placeholder="Search by tag"
+        ></input>
+        <p className="visitorHeader__inputInfo">
+          SEPARATE YOUR TAGS WITH A COMMA
+        </p>
+      </div>
     </header>
   );
 }
